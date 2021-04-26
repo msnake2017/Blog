9a1b5dc85c0f2479fcf631f9d5bc9be28cb10357
m.yarmohammadi <m.yarmohammadi@yaraholding.com>
2021-04-26 11:50:51 +0430

    chapter 14 page 288

--- articles/views.py
+++ articles/views.py
@@ -1,36 +1,42 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Article
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
 	model = Article
 	template_name = 'articles/article_list.html'
 	context_object_name = 'articles'
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
 	model = Article
 	template_name = 'articles/article_detail.html'
 	context_object_name = 'article'
 
 
-class ArticleEditView(UpdateView):
+class ArticleEditView(LoginRequiredMixin, UpdateView):
 	model = Article
 	template_name = 'articles/article_edit.html'
 	fields = ('title', 'body')
 
+	def dispatch(self, request, *args, **kwargs):
+		article = self.get_ob
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
 	model = Article
 	template_name = 'articles/article_delete.html'
 	success_url = reverse_lazy('article_list')
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
 	model = Article
 	template_name = 'articles/article_create.html'
 	fields = ('title', 'body')
+	login_url = 'login'
 
 	def form_valid(self, form):
 		form.instance.author = self.request.user
