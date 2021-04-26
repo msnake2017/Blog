958a12b5798180af1cbe9ebb313e6fa638252ac8
m.yarmohammadi <m.yarmohammadi@yaraholding.com>
2021-04-26 11:11:25 +0430

    chapter 14

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 from .models import Article
 
 
@@ -12,3 +13,25 @@ class ArticleDetailView(DetailView):
 	model = Article
 	template_name = 'articles/article_detail.html'
 	context_object_name = 'article'
+
+
+class ArticleEditView(UpdateView):
+	model = Article
+	template_name = 'articles/article_edit.html'
+	fields = ('title', 'body')
+
+
+class ArticleDeleteView(DeleteView):
+	model = Article
+	template_name = 'articles/article_delete.html'
+	success_url = reverse_lazy('article_list')
+
+
+class ArticleCreateView(CreateView):
+	model = Article
+	template_name = 'articles/article_create.html'
+	fields = ('title', 'body')
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super(ArticleCreateView, self).form_valid(form)
