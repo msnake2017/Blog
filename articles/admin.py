7ed38496ece1f2ba1f14c96e915cdd4411383b5a
m.yarmohammadi <m.yarmohammadi@yaraholding.com>
2021-04-28 12:06:41 +0430

    end of django_for_beginners

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Comment
+
+
+class CommentInline(admin.StackedInline):
+	model = Comment
+	list_display = ('author', 'comment')
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
 	list_display = ('title', 'author', 'date')
+	inlines = [CommentInline]
